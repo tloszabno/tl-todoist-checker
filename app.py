4da26722592407f1d969ce4b56c94e6b1749f48c
Tomasz Los <tloszabno@gmail.com>
2020-04-27 22:23:55 +0200

    Initial version

---
+++ app.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+
+"""
+Simple application to check Todoist inbox
+and send mail if sth is inside it
+"""
+
+import todoist
+import config
+import smtplib
+
+
+api = todoist.TodoistAPI(config.todoist_token)
+api.sync()
+
+inbox_items = filter(lambda item: item["project_id"] == config.inbox_project_id ,api.items.all())
+inbox_item_contents = list(map( lambda item: item["content"], inbox_items))
+
+
+if len(inbox_item_contents) > 0:
+    subject = "TODOIST REMINDER - You have %s task in your inbex to organize" % str(len(inbox_item_contents))
+    content = "Hello\n\n\nYou have a few task to review: \n\n-" + ("\n-".join(inbox_item_contents))
+    message = 'Subject: {}\n\n{}'.format(subject, content)
+    server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
+    server.ehlo()
+    server.login(config.gmail_user, config.gmail_password)
+    server.sendmail(config.gmail_user + "@gmail.com", config.mail_to_send_reminder, message.encode("utf8"))
+    server.close()
---
+++ config.py
@@ -0,0 +1,10 @@
+todoist_token=""
+inbox_project_id=
+
+mail_to_send_reminder=""
+
+
+
+gmail_user=""
+gmail_password=""
+
