2b54e8a1fa2438218f59d00b968d8a241e49de69
Tomasz Los <tloszabno@gmail.com>
2020-04-28 23:41:18 +0200

    fixes

--- app.py
+++ app.py
@@ -14,7 +14,8 @@ api = todoist.TodoistAPI(config.todoist_token)
 api.sync()
 
 inbox_items = filter(lambda item: item["project_id"] == config.inbox_project_id ,api.items.all())
-inbox_item_contents = list(map( lambda item: item["content"], inbox_items))
+inbox_items_unchecked = filter(lambda item: item["checked"] == 0, inbox_items)
+inbox_item_contents = list(map( lambda item: item["content"], inbox_items_unchecked))
 
 
 if len(inbox_item_contents) > 0:
